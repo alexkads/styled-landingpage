0443ca9ae34cea396e4584cef5e69773599b36e5
Alex S S Fonseca <alexkads@gmail.com>
2021-10-16 18:14:57 -0300

    Melhorias

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-interface ButtonProps {
-    children?: React.ReactNode;
+export interface ButtonProps {
+  children?: React.ReactNode;
 }
 
-export const Button: React.FC<ButtonProps> = ({children}) => {
-    return <button>{children}</button>
-}
+export const Button = ({ children }: ButtonProps) => {
+  return <button>{children}</button>;
+};
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { Button } from "./components/button";
+export { Input } from "./components/input";
